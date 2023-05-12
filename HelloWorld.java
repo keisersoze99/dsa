d43324b83afc5c19039244b037e78a2af0f859b5
Harish <keisersoze99@protonmail.com>
2023-05-12 10:19:02 +0530

    Added linear seach method

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,4 +2,15 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
   	}
+
+	public static int linearSearch(int arr[], int numberToSearch) {
+		int index = -1;
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] == numberToSearch) {
+				index = i;
+				break;
+			}
+		}
+		return index;
+	}
 }
